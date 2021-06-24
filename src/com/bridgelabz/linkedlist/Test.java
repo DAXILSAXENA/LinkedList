5233082d15383ae8624438a41d63c0a01812ce56
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-25 02:58:15 +0530

    Created a Simple Linked List

--- src/com/bridgelabz/linkedlist/Test.java
+++ src/com/bridgelabz/linkedlist/Test.java
@@ -3,5 +3,11 @@ package com.bridgelabz.linkedlist;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkedList program:");
+		Node myFirstNode = new Node(56);
+		Node mySecondNode = new Node(30);
+		Node myThirdNode = new Node(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		System.out.println(myFirstNode.getData() + "->" + mySecondNode.getData() + "->" + myThirdNode.getData());
 	}
 }
